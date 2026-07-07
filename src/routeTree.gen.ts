637a0929bdf1b51a6eb4751014179b0c1838bbf8
nyakurimwatinso-commits <nyakurimwatinso@gmail.com>
2026-07-07 18:37:01 +0200

    Update routeTree.gen.ts to define route structure

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -1,1 +1,13 @@
+/* eslint-disable */
+// @ts-nocheck
+import { FileRoute, lazyRouteComponent } from '@tanstack/react-router'
+import { Route as RootRoute } from './routes/__root'
+import { Route as IndexImport } from './routes/index'
+
+const IndexRoute = IndexImport.update({
+  path: '/',
+  getParentRoute: () => RootRoute,
+} as any)
+
+export const routeTree = RootRoute.addChildren([IndexRoute])
 
